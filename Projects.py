ecdc7abb4a5bb3f49f2aa64e2e5f9112059d49f6
ShowLowfinityHarvester <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-07-30 10:07:46 -0700

    Update Projects.py

--- Projects.py
+++ Projects.py
@@ -1,1 +1,15 @@
-print("Hello World")
+print("Madlib")
+Noun1 = input("Choose A Noun: ")
+PluralNoun1 = input("Choose A Plural Noun: ")
+Noun2 = input("Choose A Noun: ")
+NamePlace1 = input("Name A place: ")
+Adj1 = input("Choose and adjective: ")
+
+
+# Output line
+
+print("Here is your Mad lib: ")
+print("Did you Know I have a pet " +Noun1 +"?") 
+print("He likes to run around and play with all the " +PluralNoun1+ ".")
+print("One Morning, I woke up and he was wearing a " +Noun2+ " for a hat!")
+print("I especially like to take him to the " +NamePlace1+ " because he shows off his " +Adj1+ "side." )
