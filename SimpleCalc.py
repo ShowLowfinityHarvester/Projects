a2fbe7b2ce11202c8bbdc7c8ee9e9c002421447e
ShowLowfinityHarvester <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-01 10:01:19 -0700

    Create SimpleCalc.py

---
+++ SimpleCalc.py
@@ -0,0 +1,12 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
+int1 = 45
+int2 = 52
+add = int1 + int2
+subtract = int1 - int2
+multiply = int1 * int2
+divide = int1 / int2
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
