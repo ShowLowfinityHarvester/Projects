20ca4eb179eae749b3b72774d6156a77d03c9dc3
ShowLowfinityHarvester <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-01 10:25:21 -0700

    Create TeamRos.py

---
+++ TeamRos.py
@@ -0,0 +1,24 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
+Joe = 10
+
+Madeline = 32
+
+Jeremy = 29
+
+Ahmed = 19
+
+Li = 78
+
+Sarah = 90
+
+Xander = 55
+
+Jane = 72
+
+Savannah = 11
+
+Juan = 64
+
+string1 = "Welcome to the team! Your team # is {}."
+print(string1.format(Joe))
