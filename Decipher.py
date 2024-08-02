6a420937f67ff571ca5e4eb3f9a1da6c8f134855
ShowLowfinityHarvester <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-02 09:33:48 -0700

    More Projects

---
+++ Decipher.py
@@ -0,0 +1,6 @@
+Decipher = "3 pets ot og eulc txen eht dnif oT"
+string1 = Decipher
+reverse = string1
+print(reverse)
+qlen = len("I drive men mad For the love of me, Easily beaten, Never free. What Am I??")
+string2 = "Complete later"
---
+++ SentenceGen.py
@@ -0,0 +1,4 @@
+list = ["Dpg", "Mop", "Log", "Smog", "Ball", "Car", "Cat", "Golfball", "Football", "Baseball"]
+for x in list:
+    print("While I was taking my " + x + " for a walk, another giant " + x + " came out of nowhere and chased me all the way home!")
+    break
