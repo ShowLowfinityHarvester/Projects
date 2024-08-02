08ff80d9821b83ed61afcad8ea19feeea36998c1
ShowLowfinityHarvester <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-02 08:28:06 -0700

    Update TeamRos.py

--- TeamRos.py
+++ TeamRos.py
@@ -1,5 +1,25 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
+print("Joe "+ str(1))
+
+print("Madeline "+ str(1))
+
+print("Jeremy "+ str(1))
+
+print("Ahmed "+ str(1))
+
+print("Li "+ str(1))
+
+print("Sarah "+ str(1))
+
+print("Xander "+ str(1))
+
+print("Jane "+ str(1))
+
+print("Savannah "+ str(1))
+
+print("Juan "+ str(1))
+
 Joe = 10
 
 Madeline = 32
@@ -19,6 +39,5 @@ Jane = 72
 Savannah = 11
 
 Juan = 64
-
 string1 = "Welcome to the team! Your team number is {}."
 print(string1.format(Joe, Madeline, Jeremy, Ahmed, Li, Sarah, Xander, Jane, Savannah, Juan))
