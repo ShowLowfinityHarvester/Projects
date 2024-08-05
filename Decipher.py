906f9ac657cfb9b804e5f29be754a0b83de186f9
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-05 08:45:22 -0700

    Added Comments

--- Decipher.py
+++ Decipher.py
@@ -1,3 +1,5 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
 Decipher = "3 pets ot og eulc txen eht dnif oT"
 string1 = Decipher
 reverse = string1
--- SentenceGen.py
+++ SentenceGen.py
@@ -1,3 +1,5 @@
+# These are school projects! Github page may be all cluttered until 2025 or 2026.
+
 list = ["Dpg", "Mop", "Log", "Smog", "Ball", "Car", "Cat", "Golfball", "Football", "Baseball"]
 for x in list:
     print("While I was taking my " + x + " for a walk, another giant " + x + " came out of nowhere and chased me all the way home!")
