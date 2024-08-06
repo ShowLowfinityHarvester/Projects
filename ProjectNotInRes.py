d261b4061abb0df8a3bac31651eaca13ea297849
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-06 08:31:02 -0700

    Update ProjectNotInRes.py

--- ProjectNotInRes.py
+++ ProjectNotInRes.py
@@ -1,6 +1,13 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
 
-lasers = [3, 10, 4, 15, 11]
-print(lasers[2] * 10)
-print(round(3.14159265, 3))
+smells = ["skunk", "lilac", "rain", "ocean", "garbage", "cleaner", "cookies", "roses", "new car", "sweaty feet", "peach"]
+
+smells.append("apples")
+print(smells)
+
+smells.extend(["other smells", "should", "go here"])
+print(smells)
+
+smells.insert(3, "fresh cut spring grass")
+print(smells)
