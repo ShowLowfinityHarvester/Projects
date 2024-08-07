bfb975f85e2a6d3f1004eca76a2d6d0d7c4895f6
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-07 08:37:30 -0700

    Update ProjectNotInRes.py

--- ProjectNotInRes.py
+++ ProjectNotInRes.py
@@ -1,9 +1,9 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
 
-def race(Status):
-    print("You completed all laps and finished in " +Status+ " place.")
+def race(Status, CarType):
+    print("You completed all laps and finished in " +Status+ " place in your " + CarType + ".")
     
-race("1st")
-race("3rd")
-race("4th")
+race("1st", "1996 Chevy Impala ZLT V8")
+race("3rd", "2001 Mini Cooper S")
+race("4th", "2002 Ford Focus RS")
