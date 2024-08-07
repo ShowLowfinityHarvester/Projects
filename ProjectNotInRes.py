2b10ccaa500a5f8e343d844b597856e2de3b68d9
Jayson Doutree <140133382+ShowLowfinityHarvester@users.noreply.github.com>
2024-08-07 08:18:23 -0700

    Update ProjectNotInRes.py

--- ProjectNotInRes.py
+++ ProjectNotInRes.py
@@ -1,13 +1,7 @@
 # These are school projects! Github page may be all cluttered until 2025 or 2026.
 
 
-smells = ["skunk", "lilac", "rain", "ocean", "garbage", "cleaner", "cookies", "roses", "new car", "sweaty feet", "peach"]
-
-smells.append("apples")
-print(smells)
-
-smells.extend(["other smells", "should", "go here"])
-print(smells)
-
-smells.insert(3, "fresh cut spring grass")
-print(smells)
+def race():
+    print("This guy won the race!")
+    
+race()
